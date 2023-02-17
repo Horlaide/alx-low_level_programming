755424608a91b50d36788794256fedee1f1e1017
Horlaide <laidenasir@gmail.com>
2023-02-16 16:26:51 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,14 @@
 
 
 /**
- * main - "Programming is like building a multilingual puzzle
+ * main - "Programming is like building a multilingual puzzle, followed by a new line
  *
- * Return: Always 0
+ * Return: Always 0.
  */
 int main(void)
+
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0)
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
 }
